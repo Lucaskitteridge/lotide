f0b1fb7074191d992af8d58619b132569e474d15
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-24 20:35:37 +0000

    Not really sure about the JSON but it works. will look into later

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if(JSON.stringify(array1) === JSON.stringify(array2)){
+    return true
+  } else {
+    return false
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
