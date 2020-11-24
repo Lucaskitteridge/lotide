4dbeef1cd0cd1184c8380708bd23db4b69c32ace
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-24 21:10:30 +0000

    Change to easier function and linted

--- eqArrays.js
+++ eqArrays.js
@@ -7,11 +7,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  if (JSON.stringify(array1) === JSON.stringify(array2)) {
-    return true;
-  } else {
+  if (array1.length !== array2.length) {
     return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
