ac1814fc23d6385e0f63ba5b34066dddd741f43e
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-25 19:05:27 +0000

    Had to talk it out

---
+++ letterPosition.js
@@ -0,0 +1,34 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('They both match!');
+  } else {
+    console.log('The arrays dont match');
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let letters of sentence) {
+    let newArray = [];
+    for (let i = 0; i < sentence.length; i++){
+      if (sentence[i] === letters){
+        newArray.push(i)
+      }
+    }
+    results[letters] = newArray  
+  }
+  delete results[' ']
+  return results;
+};
