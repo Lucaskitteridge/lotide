b7688c392d48c994d00cfe2a9ecddca4e25c0f73
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-25 23:49:38 +0000

    linted

--- eqObjects.js
+++ eqObjects.js
@@ -19,21 +19,19 @@ const eqArrays = function(array1, array2) {
 };
 
 const eqObjects = function(object1, object2) {
-  let thing1 = Object.keys(object1)
-  let thing2 = Object.keys(object2)
+  let thing1 = Object.keys(object1);
+  let thing2 = Object.keys(object2);
   if (thing1.length !== thing2.length) {
-    return false
+    return false;
   }
   for (let key1 of thing1) {
-    if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])){
-      if(!eqArrays(object1[key1], object2[key1])){
-        return false
+    if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
+      if (!eqArrays(object1[key1], object2[key1])) {
+        return false;
       }
-
-    }
-    else if (object1[key1] !== object2[key1]) {
-      return false
+    } else if (object1[key1] !== object2[key1]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
