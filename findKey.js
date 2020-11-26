af0f9c2402f851e87fe7d791979a61ef349b4b3b
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-26 23:02:41 +0000

    linted

--- findKey.js
+++ findKey.js
@@ -1,19 +1,8 @@
 const findKey = function(object, callback) {
   for (let key in object) {
-    let value = object[key]
+    let value = object[key];
     if (callback(value)) {
-      return key
-    } 
+      return key;
+    }
   }
 };
-
-
-
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2))
