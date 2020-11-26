0d2a99d0da22c8e7b601cdfa52e7fcb721c011c4
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-26 19:41:38 +0000

    Not sure im getting it all but im getting through it

---
+++ takeUntil.js
@@ -0,0 +1,11 @@
+const takeUntil = function(array, callback) {
+  const newArray = [];
+  for (let character of array) {
+    if (!callback(character)) {
+      newArray.push(character);
+    } else {
+      break;
+    }
+  }
+  return newArray;
+};
