ffede00efc85fd543fa4c9d52e0add2134e1ea68
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-30 19:46:07 +0000

    exporting properly

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,4 @@
-const eqObjects = function(object1, object2) {
-  let thing1 = Object.keys(object1);
-  let thing2 = Object.keys(object2);
-  if (thing1.length !== thing2.length) {
-    return false;
-  }
-  for (let key1 of thing1) {
-    if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
-      if (!eqArrays(object1[key1], object2[key1])) {
-        return false;
-      }
-    } else if (object1[key1] !== object2[key1]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects')
 
 const assertObjectsEqual = function(object1, object2) {
   if (eqObjects(object1, object2)) {
@@ -24,5 +8,5 @@ const assertObjectsEqual = function(object1, object2) {
   }
 };
 
-
+module.exports = assertObjectsEqual;
 
--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const countLetters = function(string) {
   const result = {};
@@ -18,3 +12,5 @@ const countLetters = function(string) {
   delete result[' '];
   return result;
 };
+
+module.exports = countLetters
