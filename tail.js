141095b216647a8c1a71b63589d1ad90364fa736
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-30 14:38:36 +0000

    exporting to test

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.shift;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
