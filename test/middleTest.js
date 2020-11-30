17ac381c5a1da5045c67c5f97b88fac704a60205
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2020-11-30 18:44:43 +0000

    chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
+describe('#middle', () => {
 
-assertArraysEqual(middle([1, 2, 3]), [2]) 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+  it('returns an empty array for one or two values' , () => {
+    assert.deepEqual(middle([1]), [])
+    assert.deepEqual(middle([1, 2]), [])
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  it('retrurns the middle of the array when the length is odd and greater than two', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]) 
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  });
+
+  it('returns the middle two of the array when the length is even and greater then two', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+  });
+});
